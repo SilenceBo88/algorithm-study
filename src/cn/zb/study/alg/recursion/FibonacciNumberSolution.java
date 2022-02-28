e9d61ff886446738831eac4c1e90ff0027341295
zb <zhaobo@cvte.com>
2022-02-28 20:20:34 +0800

    [add][斐波那契数]

---
+++ src/cn/zb/study/alg/recursion/FibonacciNumberSolution.java
@@ -0,0 +1,65 @@
+package cn.zb.study.alg.recursion;
+
+/**
+ * @author zb
+ * @date 2022-02-28
+ * @description 斐波那契数
+ * 斐波那契数（通常用F(n) 表示）形成的序列称为 斐波那契数列 。该数列由0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+ * F(0) = 0，F(1)= 1
+ * F(n) = F(n - 1) + F(n - 2)，其中 n > 1
+ * 给定n ，请计算 F(n) 。
+ * 提示：0 <= n <= 30
+ *
+ * 输入：n = 2
+ * 输出：1
+ * 解释：F(2) = F(1) + F(0) = 1 + 0 = 1
+ *
+ * 输入：n = 3
+ * 输出：2
+ * 解释：F(3) = F(2) + F(1) = 1 + 1 = 2
+ *
+ * 输入：n = 4
+ * 输出：3
+ * 解释：F(4) = F(3) + F(2) = 2 + 1 = 3
+ */
+public class FibonacciNumberSolution {
+
+    /**
+     * 动态规划法
+     */
+    public int fib2(int n) {
+        if (n < 2) {
+            return n;
+        }
+
+        int p = 0;
+        int q = 0;
+        int r = 1;
+        for (int i = 2; i <= n; i++) {
+            p = q;
+            q = r;
+            r = p + q;
+        }
+
+        return r;
+    }
+
+    /**
+     * 递归法
+     */
+    public int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int result = new FibonacciNumberSolution().fib2(4);
+        System.out.println(result);
+    }
+}
