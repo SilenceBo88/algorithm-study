a3c31c71d89f38df96de65c551f3eecab49a2ed6
zb <zhaobo@cvte.com>
2022-02-28 20:26:15 +0800

    [add][斐波那契数]

--- src/cn/zb/study/alg/recursion/FibonacciNumberSolution.java
+++ src/cn/zb/study/alg/recursion/FibonacciNumberSolution.java
@@ -4,6 +4,7 @@ package cn.zb.study.alg.recursion;
  * @author zb
  * @date 2022-02-28
  * @description 斐波那契数
+ *
  * 斐波那契数（通常用F(n) 表示）形成的序列称为 斐波那契数列 。该数列由0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
  * F(0) = 0，F(1)= 1
  * F(n) = F(n - 1) + F(n - 2)，其中 n > 1
